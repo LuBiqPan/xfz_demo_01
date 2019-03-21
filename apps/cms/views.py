7d47ca90668346552389d647209e729de46d0ec0
LuBiqPan <xuyuanyefeng@163.com>
2019-03-21 17:26:45 +0800

    Daily update.

--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import View
+from django.views.decorators.http import require_POST, require_GET
 
-# def login_view(request):
-#     return render(request, 'cms/login.html')
+from apps.news.models import NewsCategory
+from utils import restful
 
 
 @staff_member_required(login_url='index')
 def index(request):
+
     return render(request, 'cms/index.html')
 
 
+class WriteNewsView(View):
 
+    def get(self, request):
+
+        return render(request, 'cms/write_news.html')
+
+
+@require_GET
+def news_category(request):
+    categories = NewsCategory.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'cms/news_category.html', context=context)
+
+
+@require_POST
+def add_news_category(request):
+    name = request.POST.get('name')
+    exists = NewsCategory.objects.filter(name=name).exists()
+    if not exists:
+        NewsCategory.objects.create(name=name)
+        return restful.ok()
+    else:
+        return restful.params_error(message='Category already exists.')
---
+++ front/src/js/news_category.js
@@ -0,0 +1,38 @@
+
+function NewsCategory() {
+    
+}
+
+NewsCategory.prototype.run = function () {
+    var self = this;
+    self.listenAddCategoryEvent();
+};
+
+NewsCategory.prototype.listenAddCategoryEvent = function () {
+    var addBtn = $("#add-btn");
+    addBtn.click(function () {
+        xfzalert.alertOneInput({
+            "title": "添加新闻分类",
+            "placeholder": "请输入新闻分类",
+            "confirmCallback": function (inputValue) {
+                xfzajax.post({
+                    "url": "/cms/add_news_category/",
+                    "data": {
+                        "name": inputValue
+                    },
+                    "success": function (result) {
+                        if (result["code"] === 200) {
+                            console.log(result);
+                            window.location.reload();
+                        }
+                    }
+                });
+            }
+        });
+    });
+};
+
+$(function () {
+    var newsCategory = new NewsCategory();
+    newsCategory.run();
+});
