54e578c9e73d86817bc28c50c21be76dd6ffe223
LuBiqPan <xuyuanyefeng@163.com>
2019-03-25 15:59:49 +0800

    Daily update.

--- front/src/js/write_news.js
+++ front/src/js/write_news.js
@@ -110,15 +110,36 @@ News.prototype.listenSubmitEvent = function () {
     var submitBtn = $("#submit-btn");
     submitBtn.click(function (event) {
         event.preventDefault();
+        var btn = $(this);
+        var pk = btn.attr('data-news-id');
 
         var title = $("input[name='title']").val();
-        var category = $("select[name='select']").val();
+        var category = $("select[name='category']").val();
         var desc = $("input[name='desc']").val();
         var thumbnail = $("input[name='thumbnail']").val();
         var content = window.ue.getContent();
-        console.log(content);
 
-        xfzajax({});
+        xfzajax.post({
+            "url": "/cms/write_news/",
+            "data": {
+                "title": title,
+                "category": category,
+                "desc": desc,
+                "thumbnail": thumbnail,
+                "content": content,
+                "pk": pk
+            },
+            "success": function (result) {
+                if (result["code"] === 200) {
+                    xfzalert.alertSuccess(
+                        "发布成功",
+                        function () {
+                            window.location.reload();
+                        }
+                    );
+                }
+            }
+        });
     });
 };
 
